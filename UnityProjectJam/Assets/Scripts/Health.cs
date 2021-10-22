0bb9fc1824185144d0ba3f1d2a62611d52a0534e
Ignaciomode <nachocastro62@gmail.com>
2021-10-22 17:23:30 -0300

    Sistema de cambio de Personajes
    Cada 7 segundos pasas a otro personaje

---
+++ UnityProjectJam/Assets/Scripts/Health.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int BaseHealth;
+    public int CurrentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        CurrentHealth = BaseHealth;
+    }
+    public void ReceiveDamage (int DamageReceived)
+    {
+        CurrentHealth -= DamageReceived;
+        if (CurrentHealth <= 0) { Destroy(this.gameObject); }
+
+    }
+}
---
+++ UnityProjectJam/Assets/Scripts/TurnSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnSystem : MonoBehaviour
+{
+    public static int CharacterPlaying;
+    public static float Timer;
+    public float BaseTime;
+    public int PlayerNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer -= Time.deltaTime;
+        if (Timer <= 0) { CharacterPlaying += 1; Timer = BaseTime; }
+        if (CharacterPlaying != PlayerNumber) {GetComponent<CharacterMovement>().enabled = false;}
+        if (CharacterPlaying == PlayerNumber) { GetComponent<CharacterMovement>().enabled = true;}
+        if (CharacterPlaying >= 8) { CharacterPlaying = 0; }
+    }
+}
