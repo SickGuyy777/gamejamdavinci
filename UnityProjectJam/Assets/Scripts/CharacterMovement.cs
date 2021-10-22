15276b5df1d1687ea32edf8c1861849509fe8582
Ignaciomode <nachocastro62@gmail.com>
2021-10-22 15:12:15 -0300

    Movimiento del personaje

---
+++ UnityProjectJam/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float MovementSpeed;
+    public SpriteRenderer SR;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 InputVector;
+        InputVector.x = Input.GetAxis("Horizontal");
+        InputVector.y = Input.GetAxis("Vertical");
+        InputVector.z = 0f;
+        transform.position += InputVector * Time.deltaTime * MovementSpeed;
+        if (Input.GetKey("a")) { SR.flipX = false; }
+        if (Input.GetKey("d")) { SR.flipX = true; }
+    }
+}
