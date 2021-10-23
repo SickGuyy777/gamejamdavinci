b27353f79ab5f0c4d863aeef5081a3b684672f72
Ignaciomode <nachocastro62@gmail.com>
2021-10-23 13:16:39 -0300

    Seguimiento de camara
    Ahora, la camara sigue al personaje que se esta controlando

--- UnityProjectJam/Assets/Scripts/TurnSystem.cs
+++ UnityProjectJam/Assets/Scripts/TurnSystem.cs
@@ -8,20 +8,25 @@ public class TurnSystem : MonoBehaviour
     public static float Timer;
     public float BaseTime;
     public int PlayerNumber;
+    public GameObject Target;
+    public Vector3 Cameraposition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Target = GameObject.FindGameObjectWithTag("MainCamera");
     }
 
     // Update is called once per frame
     void Update()
     {
+        Cameraposition.x = transform.position.x;
+        Cameraposition.y = transform.position.y;
+        Cameraposition.z = -10;
         Timer -= Time.deltaTime;
         if (CharacterPlaying == 8) { CharacterPlaying = 0;}
         if (Timer <= 0) { CharacterPlaying += 1; Timer = BaseTime; }
         if (CharacterPlaying != PlayerNumber) {GetComponent<CharacterMovement>().enabled = false; GetComponent<Skills>().enabled = false; }
-        if (CharacterPlaying == PlayerNumber) { GetComponent<CharacterMovement>().enabled = true; GetComponent<Skills>().enabled = true; }
-        if (CharacterPlaying == 8) { CharacterPlaying = 0; }
+        if (CharacterPlaying == PlayerNumber) { GetComponent<CharacterMovement>().enabled = true; GetComponent<Skills>().enabled = true; Target.transform.parent = transform; Target.transform.position = Cameraposition; }
+        if (CharacterPlaying == 8) { CharacterPlaying = 1; }
     }
 }
