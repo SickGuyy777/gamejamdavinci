860b815c7784bba07026d2bc31b804b919ccc3e9
Ignaciomode <nachocastro62@gmail.com>
2021-10-26 20:04:38 -0300

    Principio Enemigo

---
+++ UnityProjectJam/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float Speed;
+    public int FollowDistance;
+    public Rigidbody2D RB;
+    private Transform Target;
+    public int MaxFollowDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, Target.position) >= FollowDistance && Vector2.Distance(transform.position, Target.position) <= MaxFollowDistance) 
+        { transform.position = Vector2.MoveTowards(transform.position, Target.position, Speed * Time.deltaTime); }
+        Vector3 Direction = Target.position - transform.position;
+        float angle = Mathf.Atan2(Direction.y, Direction.x) * Mathf.Rad2Deg;
+        RB.rotation = angle - 90;
+    }
+}
---
+++ UnityProjectJam/Assets/Scripts/EnemyProjectileSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectileSpawner : MonoBehaviour
+{
+    public float CurrentAttackCooldown;
+    public float AttackCooldown;
+    private Transform Target;
+    public float AttackRange;
+    public GameObject Bala;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CurrentAttackCooldown -= Time.deltaTime; 
+        if (Vector2.Distance(transform.position, Target.position) <= AttackRange && CurrentAttackCooldown <= 0)
+        {
+            GameObject BalaInstance = Instantiate(Bala, transform.position, Quaternion.identity); CurrentAttackCooldown = AttackCooldown;
+            BalaInstance.transform.right = transform.up;
+        }
+    }
+}
