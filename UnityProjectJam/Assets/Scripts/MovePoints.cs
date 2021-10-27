4128a29854f5c962ed910cd8107a2de994dd15e4
SickGuyy777 <57241415+KAWkkkk@users.noreply.github.com>
2021-10-27 18:22:51 -0300

    Enemigo goma

---
+++ UnityProjectJam/Assets/Scripts/MovePoints.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePoints : MonoBehaviour
+{
+    public Transform pointA;
+    public Transform pointB;
+    public Transform currentTarget;
+    public float movementSpeed;
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        currentTarget = pointA;
+    }
+
+    void Update()
+    {
+        rb.MovePosition(Vector2.MoveTowards(rb.position, currentTarget.position, movementSpeed * Time.deltaTime));
+        if (Vector2.Distance(rb.position, currentTarget.position) == 0)
+        {
+            if (currentTarget == pointA)
+            {
+                currentTarget = pointB;
+            }
+            else
+            {
+                currentTarget = pointA;
+            }
+        }
+    }
+}
